aca2c00089bdeeded22c20a0258a51784062106f
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-22 17:20:42 -0600

    implement comparable with java7

--- api/src/main/java/mx/com/service/BiFunctionService.java
+++ api/src/main/java/mx/com/service/BiFunctionService.java
@@ -1,5 +1,8 @@
 package mx.com.service;
 
+import java.util.List;
+
 public interface BiFunctionService {
   public Integer firstExample(Integer firstNumber, Integer secondNumber);
+  public List<String> ordenarJava7(List<String> lista);
 }
