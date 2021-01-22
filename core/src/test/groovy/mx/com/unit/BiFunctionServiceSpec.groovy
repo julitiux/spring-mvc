f31f417eaaeb651780d7302ffb1a1fb3a95f3891
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-22 17:21:37 -0600

    implement unit test comparable java7

--- core/src/test/groovy/mx/com/unit/BiFunctionServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/BiFunctionServiceSpec.groovy
@@ -24,4 +24,21 @@ class BiFunctionServiceSpec extends Specification {
     0            | 0             | 0
   }
 
+  def "ordenar Java7 ordenando listas #_lista de #_result"() {
+    given:
+    BiFunctionService biFunctionService = new BiFunctionServiceImpl();
+    List<String> lista = _lista
+//    List<String> lista = Arrays.asList("5", "6", "3", "7", "2");  JAVA7
+//    List<String> lista = ["5","6","3","7","2"]   GROOVY
+    List<String> listaOrdenanda
+    when:
+    listaOrdenanda = biFunctionService.ordenarJava7(lista);
+    then:
+    listaOrdenanda == _result
+    where:
+    _lista                    | _result
+    ["5", "6", "3", "7", "2"] | ["2", "3", "5", "6", "7"]
+    ["5"]                     | ["5"]
+  }
+
 }
