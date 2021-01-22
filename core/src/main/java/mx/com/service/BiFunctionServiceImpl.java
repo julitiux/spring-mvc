01eb9f35de11c3500317e6019627dd89b8377af0
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-22 17:21:13 -0600

    implement comparable with java7

--- core/src/main/java/mx/com/service/BiFunctionServiceImpl.java
+++ core/src/main/java/mx/com/service/BiFunctionServiceImpl.java
@@ -1,6 +1,11 @@
 package mx.com.service;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiFunction;
 
 @Service
@@ -10,6 +15,17 @@ public class BiFunctionServiceImpl implements BiFunctionService {
   public Integer firstExample(Integer firstNumber, Integer secondNumber) {
     BiFunction<Integer, Integer, Integer> function = (first, second) -> first + second;
     return function.apply(firstNumber, secondNumber);
+  }
+
+  @Override
+  public List<String> ordenarJava7(List<String> lista) {
+    Collections.sort(lista, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return o1.compareTo(o2);
+      }
+    });
+    return lista;
   }
 
 }
