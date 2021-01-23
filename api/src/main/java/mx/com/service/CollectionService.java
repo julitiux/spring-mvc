14b38a64916474417bf97b35a703eb29bfec015b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-22 20:41:57 -0600

    implement method forEach and remove in java 7 and 8

--- api/src/main/java/mx/com/service/CollectionService.java
+++ api/src/main/java/mx/com/service/CollectionService.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface CollectionService {
   public void forEachJava7(List<String> list);
   public void forEachJava8(List<String> list);
+  public void forEachJava8_1(List<String> list);
+
+  public List<String> useRemoveIfJava7(List<String> list, String elementRemove);
+  public List<String> useRemoveIfJava8(List<String> list, String elementRemove);
 }
--- core/src/main/java/mx/com/service/CollectionServiceImpl.java
+++ core/src/main/java/mx/com/service/CollectionServiceImpl.java
@@ -2,6 +2,9 @@ package mx.com.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -18,4 +21,26 @@ public class CollectionServiceImpl implements CollectionService {
   public void forEachJava8(List<String> list) {
     list.forEach(it -> System.out.println(it));
   }
+
+  @Override
+  public void forEachJava8_1(List<String> list) {
+    list.forEach(System.out::println);
+  }
+
+  @Override
+  public List<String> useRemoveIfJava7(List<String> list, String elementRemove) {
+    Iterator<String> iterator = list.iterator();
+    while(iterator.hasNext()){
+      if(iterator.next().equalsIgnoreCase(elementRemove))
+        iterator.remove();
+    }
+    return list;
+  }
+
+  @Override
+  public List<String> useRemoveIfJava8(List<String> list, String elementRemove) {
+    list.removeIf(x-> x.equalsIgnoreCase(elementRemove));
+    return list;
+  }
+  
 }
