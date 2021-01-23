a05a66a63bbdda7b27276a03b5a1b98a7de6d92b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-22 20:42:28 -0600

    implement unit test of remove and forEach in Java 7 and 8

--- core/src/test/groovy/mx/com/unit/CollectionServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/CollectionServiceSpec.groovy
@@ -6,10 +6,10 @@ import spock.lang.Specification
 
 class CollectionServiceSpec extends Specification {
 
-  def"For Each java7"(){
+  def "For Each java7"() {
     given:
     CollectionService collectionService = new CollectionServiceImpl()
-    List<String> list = ['1','2','3','4','5','6']
+    List<String> list = ['1', '2', '3', '4', '5', '6']
     when:
     collectionService.forEachJava7(list)
     then:
@@ -17,14 +17,51 @@ class CollectionServiceSpec extends Specification {
   }
 
 
-  def"For Each java8"(){
+  def "For Each java8"() {
     given:
     CollectionService collectionService = new CollectionServiceImpl()
-    List<String> list = ['1','2','3','4','5','6']
+    List<String> list = ['1', '2', '3', '4', '5', '6']
     when:
     collectionService.forEachJava8(list)
     then:
     true
+  }
+
+  def "For Each java8_1 "() {
+    given:
+    CollectionService collectionService = new CollectionServiceImpl()
+    List<String> list = ['1', '2', '3', '4', '5', '6']
+    when:
+    collectionService.forEachJava8_1(list)
+    then:
+    true
+  }
+
+
+  def "Iterator delete one element of a list with java 7"() {
+    given:
+    CollectionService collectionService = new CollectionServiceImpl()
+    List<String> list = _list
+    when:
+    list = collectionService.useRemoveIfJava7(list, _elementRemove)
+    then:
+    list == _result
+    where:
+    _list                          | _elementRemove | _result
+    ['1', '2', '3', '4', '5', '6'] | '1'            | ['2', '3', '4', '5', '6']
+  }
+
+  def "Iterator delete one element of a list with java 8"() {
+    given:
+    CollectionService collectionService = new CollectionServiceImpl()
+    List<String> list = _list
+    when:
+    list = collectionService.useRemoveIfJava8(list, _elementRemove)
+    then:
+    list == _result
+    where:
+    _list                          | _elementRemove | _result
+    ['1', '2', '3', '4', '5', '6'] | '6'            | ['1', '2', '3', '4', '5']
   }
 
 }
