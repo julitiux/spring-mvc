ba83f8afbc9214453157aa25f24ca00a523bb54d
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-25 14:15:38 -0600

    add sintaxis return at all method

--- core/src/main/java/mx/com/service/StreamServiceImpl.java
+++ core/src/main/java/mx/com/service/StreamServiceImpl.java
@@ -8,26 +8,27 @@ import java.util.List;
 public class StreamServiceImpl implements StreamService{
   @Override
   public List<String> filter(List<String> list) {
-
+    list.stream().filter(x -> x.startsWith("M"));
+    return list;
   }
 
   @Override
   public List<String> sorter(List<String> list) {
-
+    return list;
   }
 
   @Override
   public List<String> transformer(List<String> list) {
-
+    return list;
   }
 
   @Override
   public List<String> limiter(List<String> list) {
-
+    return list;
   }
 
   @Override
   public List<String> counter(List<String> list) {
-
+    return list;
   }
 }
