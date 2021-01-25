5c8d5113c3075b2a45f74b4a2ab9c90f8f37df07
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-25 14:16:22 -0600

    delete all the implementations

--- core/src/test/groovy/mx/com/unit/StreamServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/StreamServiceSpec.groovy
@@ -3,20 +3,5 @@ package mx.com.unit
 import spock.lang.Specification
 
 class StreamServiceSpec extends Specification {
-
-  def"Test"(){
-    given:
-    String oneVariable
-    when:
-    //Execyte something
-    oneVariable = 'Assagment a string value'
-    then:
-    //Something happend
-    println oneVariable
-//    where:
-//    just variables
-
-
-  }
-
+  
 }
