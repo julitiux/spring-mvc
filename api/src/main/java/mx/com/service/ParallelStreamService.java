316df85424126b7b5a61146ad86e7f5322d37a38
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-27 15:49:54 -0600

    declare and implement empty methods of Parallel Streams

---
+++ api/src/main/java/mx/com/service/ParallelStreamService.java
@@ -0,0 +1,9 @@
+package mx.com.service;
+
+import java.util.List;
+
+public interface ParallelStreamService {
+  public List<String> addList();
+  public List<String> testStream();
+  public List<String> testParallel();
+}
---
+++ core/src/main/java/mx/com/service/ParallelStreamServiceImpl.java
@@ -0,0 +1,24 @@
+package mx.com.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ParallelStreamServiceImpl implements ParallelStreamService{
+
+  @Override
+  public List<String> addList() {
+    return null;
+  }
+
+  @Override
+  public List<String> testStream() {
+    return null;
+  }
+
+  @Override
+  public List<String> testParallel() {
+    return null;
+  }
+}
