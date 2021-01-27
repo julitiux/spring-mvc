4289c8e28825bf4ef14e27efea0394ff986bd786
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-27 15:51:14 -0600

    delete the method addList. Change the name testParallel to testParallelStream

--- api/src/main/java/mx/com/service/ParallelStreamService.java
+++ api/src/main/java/mx/com/service/ParallelStreamService.java
@@ -3,7 +3,6 @@ package mx.com.service;
 import java.util.List;
 
 public interface ParallelStreamService {
-  public List<String> addList();
   public List<String> testStream();
-  public List<String> testParallel();
+  public List<String> testParallelStream();
 }
