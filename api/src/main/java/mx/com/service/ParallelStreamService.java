da1411ef151086d8b0a92963975535bebdd0a2c0
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-27 16:03:58 -0600

    modify the return object. I replace the List<String> with a void.

--- api/src/main/java/mx/com/service/ParallelStreamService.java
+++ api/src/main/java/mx/com/service/ParallelStreamService.java
@@ -3,6 +3,6 @@ package mx.com.service;
 import java.util.List;
 
 public interface ParallelStreamService {
-  public List<String> testStream(List<String> list);
-  public List<String> testParallelStream(List<String> list);
+  public void testStream(List<String> list);
+  public void testParallelStream(List<String> list);
 }
--- core/src/main/java/mx/com/service/ParallelStreamServiceImpl.java
+++ core/src/main/java/mx/com/service/ParallelStreamServiceImpl.java
@@ -2,18 +2,19 @@ package mx.com.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ParallelStreamServiceImpl implements ParallelStreamService{
 
   @Override
-  public List<String> testStream(List<String> list) {
-    return null;
+  public void testStream(List<String> list) {
+    
   }
 
   @Override
-  public List<String> testParallelStream(List<String> list) {
-    return null;
+  public void testParallelStream(List<String> list) {
+
   }
 }
