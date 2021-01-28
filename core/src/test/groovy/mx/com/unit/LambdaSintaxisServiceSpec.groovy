f399a1aeb841812b602a58818e59f104d6240977
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-28 15:48:59 -0600

    Implement unit test with sintaxis2 lambda #_number1 | #_number2 | #_response

--- core/src/test/groovy/mx/com/unit/LambdaSintaxisServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/LambdaSintaxisServiceSpec.groovy
@@ -22,4 +22,20 @@ class LambdaSintaxisServiceSpec extends Specification {
     2        | 3        | 2.5
 
   }
+
+  @Unroll
+  def "Implement unit test with sintaxis2 lambda #_number1 | #_number2 | #_response"() {
+    given:
+    LambdaSintaxisServiceImpl clazz = new LambdaSintaxisServiceImpl()
+    Double number1 = _number1
+    Double number2 = _number2
+    Double response
+    when:
+    response = clazz.sintaxis2(number1, number2)
+    then:
+    response == _response
+    where:
+    _number1 | _number2 | _response
+    2        | 3        | 2.5
+  }
 }
