b38a27a22e0b4e866509d68320445afb2e965a3e
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-28 15:34:57 -0600

    Implement unit test of the diference Imperative and declarative programming #_number1 | #_number2 | #_response

---
+++ core/src/test/groovy/mx/com/unit/LambdaServiceSpec.groovy
@@ -0,0 +1,25 @@
+package mx.com.unit
+
+import mx.com.service.LambdaServiceImpl
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class LambdaServiceSpec extends Specification {
+
+  @Unroll
+  def "Implement unit test of the diference Imperative and declarative programming #_number1 | #_number2 | #_response"() {
+    given:
+    LambdaServiceImpl clazz = new LambdaServiceImpl()
+    Double number1 = _number1
+    Double number2 = _number2
+    Double response
+    when:
+    response = clazz.calculateImperative(number1, number2)
+    then:
+    response == _response
+    where:
+    _number1 | _number2 | _response
+    2        | 3        | 2.5
+
+  }
+}
