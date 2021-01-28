e6fd320aeee9b8cd5dc65f7396866853dd98725c
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-28 15:34:41 -0600

    modify the object return, void to Double

--- core/src/main/java/mx/com/service/LambdaServiceImpl.java
+++ core/src/main/java/mx/com/service/LambdaServiceImpl.java
@@ -3,7 +3,7 @@ package mx.com.service;
 public class LambdaServiceImpl {
 
 //  Imperative programming
-  public void calculateImperative(Double num1, Double num2) {
+  public Double calculateImperative(Double num1, Double num2) {
     LambdaService lambdaService = new LambdaService() {
       @Override
       public Double calculateAverage(Double num1, Double num2) {
@@ -11,14 +11,14 @@ public class LambdaServiceImpl {
       }
     };
 
-    System.out.println(lambdaService.calculateAverage(num1, num2));
+    return lambdaService.calculateAverage(num1, num2);
 
   }
 
 //  Declarative programming
-  public void calculateDeclarative(Double num1, Double num2) {
+  public Double calculateDeclarative(Double num1, Double num2) {
     LambdaService lambdaService = (Double n1, Double n2) -> (n1 + n2) / 2;
-    System.out.println(lambdaService.calculateAverage(num1, num2));
+    return lambdaService.calculateAverage(num1, num2);
   }
 
 }
