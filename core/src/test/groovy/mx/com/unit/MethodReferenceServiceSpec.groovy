35f7697b04a0c363d032e9e31d0f88f57e828466
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-29 14:28:35 -0600

    Implement unit test of reference Method Static

---
+++ core/src/test/groovy/mx/com/unit/MethodReferenceServiceSpec.groovy
@@ -0,0 +1,16 @@
+package mx.com.unit
+
+import mx.com.service.MethodReferenceServiceImpl
+import spock.lang.Specification
+
+class MethodReferenceServiceSpec extends Specification{
+
+  def"Implement unit test of reference Method Static"(){
+    given:
+    MethodReferenceServiceImpl instance = new MethodReferenceServiceImpl()
+    when:
+    instance.example1()
+    then:
+    true
+  }
+}
