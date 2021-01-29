fbd2ad30f8a8a1de186e5086dc7e7aeba0321814
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-29 17:29:49 -0600

    implement a domain User

---
+++ api/src/main/java/mx/com/domain/User.java
@@ -0,0 +1,24 @@
+package mx.com.domain;
+
+public class User {
+
+  Long id;
+  String username;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+}
