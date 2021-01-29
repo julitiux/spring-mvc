91ba55d80f84d855f8adcf67948511e89fcc4bf5
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-29 13:35:43 -0600

    modify the comment in the interface FunctionalInterfaceService

--- api/src/main/java/mx/com/service/FunctionalInterfaceService.java
+++ api/src/main/java/mx/com/service/FunctionalInterfaceService.java
@@ -2,6 +2,6 @@ package mx.com.service;
 
 @FunctionalInterface
 public interface FunctionalInterfaceService {
-//  Es aquella que define solo una implementacion, Java lo presenta con la anotacionFunctionalInterface. Basicamente no puedes colocar dos metodos
+//  Es aquella que define solo una implementacion, Java lo presenta con la anotacion FunctionalInterface. Basicamente no puedes colocar dos metodos en la interface
   public void process();
 }
