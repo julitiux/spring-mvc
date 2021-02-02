20f45a15cbdfe1fb3ecac99a2fad8cabb396ae35
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-02 17:21:49 -0600

    delete all the import usless. I ident the code

--- core/src/main/java/mx/com/service/ParallelStreamServiceImpl.java
+++ core/src/main/java/mx/com/service/ParallelStreamServiceImpl.java
@@ -2,12 +2,10 @@ package mx.com.service;
 
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLOutput;
-import java.util.Collections;
 import java.util.List;
 
 @Service
-public class ParallelStreamServiceImpl implements ParallelStreamService{
+public class ParallelStreamServiceImpl implements ParallelStreamService {
 
   @Override
   public void testStream(List<String> list) {
