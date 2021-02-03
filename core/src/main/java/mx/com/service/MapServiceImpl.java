4d71f4c2d9690aa40e981dd0270179f8df7f62c5
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 13:58:17 -0600

    add empty that will be implementated

---
+++ core/src/main/java/mx/com/service/MapServiceImpl.java
@@ -0,0 +1,31 @@
+package mx.com.service;
+
+import java.util.Map;
+
+public class MapServiceImpl implements MapService {
+
+  @Override
+  public Map<Integer, String> printJava7(Map<Integer, String> map) {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, String> printJava8(Map<Integer, String> map) {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, String> collect(Map<Integer, String> map) {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, String> insertIfAbsent(Map<Integer, String> map) {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, String> operateIfPresent() {
+    return null;
+  }
+}
