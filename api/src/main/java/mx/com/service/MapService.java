b92afadb43a01b8ccae95cbede69c44f2143b8ee
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 17:33:02 -0600

    modify the method operateIfPresent add like a parameter map

--- api/src/main/java/mx/com/service/MapService.java
+++ api/src/main/java/mx/com/service/MapService.java
@@ -8,5 +8,5 @@ public interface MapService {
   public void printJava8_1(Map<Integer, String> map);
   public Map<Integer, String> collect(Map<Integer, String> map);
   public Map<Integer, String> insertIfAbsent(Map<Integer, String> map, Integer key, String value);
-  public Map<Integer, String> operateIfPresent();
+  public Map<Integer, String> operateIfPresent(Map<Integer, String> map);
 }
