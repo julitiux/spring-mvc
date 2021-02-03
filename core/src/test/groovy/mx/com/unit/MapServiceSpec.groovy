5745789e946b63803ff4f3e40831e6e99c0c4fed
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 15:56:34 -0600

    add each unit test implementation

--- core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
@@ -14,4 +14,22 @@ class MapServiceSpec extends Specification {
     service.printJava7(map);
   }
 
+  def"Implementation a unit test of print a map with Java8 BiConsumer"(){
+    given:
+    Map map = [1:'uno', 2:'dos', 3:'tres', 4:'cuatro', 5:'cinco']
+    MapService service = new MapServiceImpl()
+    expect:
+    service.printJava8(map);
+  }
+
+  def"Implementation a unit test of print a map with Java8 Consumer"(){
+    given:
+    Map map = [1:'uno', 2:'dos', 3:'tres', 4:'cuatro', 5:'cinco']
+    MapService service = new MapServiceImpl()
+    expect:
+    service.printJava8_1(map);
+  }
+
+
+
 }
