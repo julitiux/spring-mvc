fb9f5303ebf286842550ea5b9807e40f81b042ff
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 15:55:15 -0600

    declare new method printJava8_1 and change the return object with a void

--- api/src/main/java/mx/com/service/MapService.java
+++ api/src/main/java/mx/com/service/MapService.java
@@ -4,7 +4,8 @@ import java.util.Map;
 
 public interface MapService {
   public void printJava7(Map<Integer, String> map);
-  public Map<Integer, String> printJava8(Map<Integer, String> map);
+  public void printJava8(Map<Integer, String> map);
+  public void printJava8_1(Map<Integer, String> map);
   public Map<Integer, String> collect(Map<Integer, String> map);
   public Map<Integer, String> insertIfAbsent(Map<Integer, String> map);
   public Map<Integer, String> operateIfPresent();
