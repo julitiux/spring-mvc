82eabc311dc0bda24200a9cd85819dfb4e769d37
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 16:12:24 -0600

    add Implementation a unit test of method putIfAbsent

--- core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
@@ -6,30 +6,47 @@ import spock.lang.Specification
 
 class MapServiceSpec extends Specification {
 
-  def"Implementation a unit test of print a map"(){
+  def "Implementation a unit test of print a map"() {
     given:
-    Map map = [1:'uno', 2:'dos', 3:'tres', 4:'cuatro', 5:'cinco']
+    Map map = [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
     MapService service = new MapServiceImpl()
     expect:
     service.printJava7(map);
   }
 
-  def"Implementation a unit test of print a map with Java8 BiConsumer"(){
+  def "Implementation a unit test of print a map with Java8 BiConsumer"() {
     given:
-    Map map = [1:'uno', 2:'dos', 3:'tres', 4:'cuatro', 5:'cinco']
+    Map map = [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
     MapService service = new MapServiceImpl()
     expect:
     service.printJava8(map);
   }
 
-  def"Implementation a unit test of print a map with Java8 Consumer"(){
+  def "Implementation a unit test of print a map with Java8 Consumer"() {
     given:
-    Map map = [1:'uno', 2:'dos', 3:'tres', 4:'cuatro', 5:'cinco']
+    Map map = [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
     MapService service = new MapServiceImpl()
     expect:
     service.printJava8_1(map);
   }
 
+
+  def "Implementation a unit test of method putIfAbsent"() {
+    given:
+    Map map = [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
+    Map reponse
+    MapService service = new MapServiceImpl()
+    when:
+    reponse = service.insertIfAbsent(map);
+    then:
+    reponse == map
+
+    where:
+    _map                                                     | _response
+    [1: 'uno', 3: 'tres']                                    | [1: 'uno', 3: 'tres', 2: 'dos dos']
+    [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco'] | [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
+
+  }
 
 
 }
