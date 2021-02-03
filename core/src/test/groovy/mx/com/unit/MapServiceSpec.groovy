460798d7f9a7b7b4c1a09c1020ecea4aa44e387b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 17:28:05 -0600

    modify the unit test implementation of the method putIfAbsent. Add Unroll

--- core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/MapServiceSpec.groovy
@@ -3,7 +3,9 @@ package mx.com.unit
 import mx.com.service.MapService
 import mx.com.service.MapServiceImpl
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class MapServiceSpec extends Specification {
 
   def "Implementation a unit test of print a map"() {
@@ -31,20 +33,22 @@ class MapServiceSpec extends Specification {
   }
 
 
-  def "Implementation a unit test of method putIfAbsent"() {
+  def "Implementation a unit test of method putIfAbsent using #_map | #_response "() {
     given:
-    Map map = [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
+    Map map = _map
+    Integer key = _key
+    String value = _value
     Map reponse
     MapService service = new MapServiceImpl()
     when:
-    reponse = service.insertIfAbsent(map);
+    reponse = service.insertIfAbsent(map, key, value);
     then:
-    reponse == map
+    reponse == _response
 
     where:
-    _map                                                     | _response
-    [1: 'uno', 3: 'tres']                                    | [1: 'uno', 3: 'tres', 2: 'dos dos']
-    [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco'] | [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
+    _map                                                     | _key | _value    | _response
+    [1: 'uno', 3: 'tres']                                    | 2    | 'dos dos' | [1: 'uno', 3: 'tres', 2: 'dos dos']
+    [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco'] | 5    | null      | [1: 'uno', 2: 'dos', 3: 'tres', 4: 'cuatro', 5: 'cinco']
 
   }
 
