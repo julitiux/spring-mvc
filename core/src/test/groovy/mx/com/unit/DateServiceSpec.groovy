542fa192ae7cdbf637a11ed1765963e48f0261bd
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-04 17:15:00 -0600

    implement a test of LocalDate Function

--- core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
@@ -5,6 +5,8 @@ import mx.com.service.DateServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
 
+import java.time.LocalDate
+
 class DateServiceSpec extends Specification {
 
   @Shared
@@ -13,6 +15,14 @@ class DateServiceSpec extends Specification {
   def "Just execute the implementation of method dateJava7"() {
     expect:
     service.dateJava7()
+  }
+
+  def "Implement a test of LocalDate function"() {
+    given:
+    LocalDate startDate = LocalDate.of(1984,5,19)
+    LocalDate endDate = LocalDate.now();
+    expect:
+    !startDate.isAfter(endDate)
   }
 
 }
