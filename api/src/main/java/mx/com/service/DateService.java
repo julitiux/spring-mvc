3e900dca2093fcb55920aa12e4bbd7222631be5c
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-04 17:30:42 -0600

    declare method comparteTwoTimesInJava8

--- api/src/main/java/mx/com/service/DateService.java
+++ api/src/main/java/mx/com/service/DateService.java
@@ -1,9 +1,11 @@
 package mx.com.service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public interface DateService {
   public void dateJava7();
   public Boolean comparteTwoDatesInJava8(LocalDate startDate, LocalDate endDate);
+  public Boolean comparteTwoTimesInJava8(LocalTime startTime, LocalTime endTime);
 
 }
