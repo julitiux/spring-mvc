bcc284160a93ee28d41f22c990bbf1b8c8efad54
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-03 18:05:11 -0600

    add new method collect_v1 and v2. Adding value like String

--- api/src/main/java/mx/com/service/MapService.java
+++ api/src/main/java/mx/com/service/MapService.java
@@ -6,7 +6,8 @@ public interface MapService {
   public void printJava7(Map<Integer, String> map);
   public void printJava8(Map<Integer, String> map);
   public void printJava8_1(Map<Integer, String> map);
-  public Map<Integer, String> collect(Map<Integer, String> map);
+  public Map<Integer, String> collect_v1(Map<Integer, String> map, String value);
+  public Map<Integer, String> collect_v2(Map<Integer, String> map, String value);
   public Map<Integer, String> insertIfAbsent(Map<Integer, String> map, Integer key, String value);
   public Map<Integer, String> operateIfPresent(Map<Integer, String> map, Integer key, String value);
   public String getOrDefault(Map<Integer, String> map, Integer key);
