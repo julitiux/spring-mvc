cba00e9ee5d5e2cb14515ac7ad50b2f84d0001d3
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-04 17:24:06 -0600

    Implement a test unit of the method comparteTwoDatesInJava8

--- core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
@@ -4,9 +4,11 @@ package mx.com.unit
 import mx.com.service.DateServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.time.LocalDate
 
+@Unroll
 class DateServiceSpec extends Specification {
 
   @Shared
@@ -17,9 +19,27 @@ class DateServiceSpec extends Specification {
     service.dateJava7()
   }
 
+  def "Implement a test unit of the method comparteTwoDatesInJava8"() {
+    given:
+    LocalDate startDate = _startDate
+    LocalDate endtDate = _endDate
+    Boolean response
+    when:
+    response = service.comparteTwoDatesInJava8(startDate, endtDate)
+    then:
+    response == _response
+    where:
+    _startDate                 | _endDate                   | _response
+    LocalDate.of(19984, 5, 19) | LocalDate.now()            | false
+    LocalDate.now()            | LocalDate.of(19984, 5, 19) | true
+    LocalDate.now()            | LocalDate.now()            | false
+    LocalDate.MAX              | LocalDate.MIN              | false
+    LocalDate.MIN              | LocalDate.MAX              | true
+  }
+
   def "Implement a test of LocalDate function"() {
     given:
-    LocalDate startDate = LocalDate.of(1984,5,19)
+    LocalDate startDate = LocalDate.of(1984, 5, 19)
     LocalDate endDate = LocalDate.now();
     expect:
     !startDate.isAfter(endDate)
