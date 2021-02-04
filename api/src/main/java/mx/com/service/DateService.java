1de0d2e94c4d50ea89893ad3d2d184c5bab6e9c3
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-04 17:15:18 -0600

    add new method with two LocaDate for params

--- api/src/main/java/mx/com/service/DateService.java
+++ api/src/main/java/mx/com/service/DateService.java
@@ -1,5 +1,9 @@
 package mx.com.service;
 
+import java.time.LocalDate;
+
 public interface DateService {
   public void dateJava7();
+  public Boolean comparteTwoDatesInJava8(LocalDate startDate, LocalDate endDate);
+
 }
