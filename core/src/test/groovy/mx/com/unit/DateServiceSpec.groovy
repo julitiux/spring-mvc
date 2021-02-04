0c005e13c8635867aa7ca98dde56526cead4b433
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-04 17:27:44 -0600

    Same test before but using expect

--- core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
+++ core/src/test/groovy/mx/com/unit/DateServiceSpec.groovy
@@ -37,6 +37,22 @@ class DateServiceSpec extends Specification {
     LocalDate.MIN              | LocalDate.MAX              | true
   }
 
+  def "Same test before but using expect"() {
+    given:
+    LocalDate startDate = _startDate
+    LocalDate endDate = _endDate
+    expect:
+    startDate.isBefore(endDate) == _response
+    where:
+    _startDate                 | _endDate                   | _response
+    LocalDate.of(19984, 5, 19) | LocalDate.now()            | false
+    LocalDate.now()            | LocalDate.of(19984, 5, 19) | true
+    LocalDate.now()            | LocalDate.now()            | false
+    LocalDate.MAX              | LocalDate.MIN              | false
+    LocalDate.MIN              | LocalDate.MAX              | true
+
+  }
+
   def "Implement a test of LocalDate function"() {
     given:
     LocalDate startDate = LocalDate.of(1984, 5, 19)
