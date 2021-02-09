b743d3323b8e5b9ccafb7d4874b4206e4acbe8ef
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-08 19:23:55 -0600

    declare a new method observableRxWithLambdas

--- api/src/main/java/mx/com/service/JavaRxService.java
+++ api/src/main/java/mx/com/service/JavaRxService.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface JavaRxService {
   public void observableRx(List<String> list);
   public List<String> observableRx(List<String> firstList, List<String> secondList, String string);
+  public List<String> observableRxWithLambdas(List<String> firstList, List<String> secondList, String string);
 }
