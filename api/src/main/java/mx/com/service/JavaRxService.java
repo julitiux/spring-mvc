f0eb5f22f1c156091a783cfb7add95b44f0d66d3
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-08 18:58:53 -0600

    modify the object return void by List<String> on the method observableRx

--- api/src/main/java/mx/com/service/JavaRxService.java
+++ api/src/main/java/mx/com/service/JavaRxService.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface JavaRxService {
   public void observableRx(List<String> list);
+  public List<String> observableRx(List<String> firstList, List<String> secondList, String string);
 }
