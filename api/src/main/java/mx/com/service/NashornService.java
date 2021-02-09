62cd08569fb36c03fbd519b05e38b1863f965696
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-09 15:48:35 -0600

    declare another method called callFunctionsWithFileJs

--- api/src/main/java/mx/com/service/NashornService.java
+++ api/src/main/java/mx/com/service/NashornService.java
@@ -2,5 +2,6 @@ package mx.com.service;
 
 public interface NashornService {
   public void callFunctions() throws Exception;
+  public void callFunctionsWithFileJs() throws Exception;
   public void implementInterface() throws Exception;
 }
