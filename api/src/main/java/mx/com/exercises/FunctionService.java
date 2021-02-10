8bfa3519ee9c52380f8f9178d6bd2d361e4988a8
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-09 23:31:08 -0600

    declare a method listToMap, recibe a List a return a Map xD

--- api/src/main/java/mx/com/exercises/FunctionService.java
+++ api/src/main/java/mx/com/exercises/FunctionService.java
@@ -1,6 +1,10 @@
 package mx.com.exercises;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FunctionService {
   public Integer functionLenght(String string);
   public Integer functionAndThen(String string);
+  public Map<String, Integer> listToMap(List<String> list);
 }
