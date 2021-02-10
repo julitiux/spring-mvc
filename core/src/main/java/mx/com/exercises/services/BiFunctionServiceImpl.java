454a840c80389e8265dd92cbb10d1e34c9f568dd
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-10 14:54:07 -0600

    implementa private method powFunction. And implement a method public pow, using the private method powFunction and apply

--- core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
+++ core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
@@ -5,9 +5,15 @@ import java.util.function.BiFunction;
 public class BiFunctionServiceImpl implements BiFunctionService{
 
   private BiFunction<Integer, Integer, Integer> addBiFunction = (firstNumber, secondNumber) -> firstNumber+secondNumber;
+  private BiFunction<Integer, Integer, Double> powFunction = (firstNumber, secondNumber) -> Math.pow(firstNumber,secondNumber);
 
   @Override
   public Integer add(Integer firstNumber, Integer secondNumber) {
     return addBiFunction.apply(firstNumber, secondNumber);
+  }
+
+  @Override
+  public Double pow(Integer firstNumber, Integer secondNumber) {
+    return powFunction.apply(firstNumber,secondNumber);
   }
 }
