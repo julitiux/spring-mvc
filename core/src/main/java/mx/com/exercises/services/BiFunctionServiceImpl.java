ca7d24940e32bfec09c159adb7f45ec9760db1b2
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-10 15:16:13 -0600

    implement a method powAndTurnString using a BiFunction with a Function join with a andThen

--- core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
+++ core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class BiFunctionServiceImpl implements BiFunctionService{
 
   private BiFunction<Integer, Integer, Integer> addBiFunction = (firstNumber, secondNumber) -> firstNumber+secondNumber;
   private BiFunction<Integer, Integer, Double> powFunction = (firstNumber, secondNumber) -> Math.pow(firstNumber,secondNumber);
   private BiFunction<Integer, Integer, List<String>> sumAPairNumbertoList = (firstNumber, secondNumber) -> Arrays.asList(String.valueOf(firstNumber + secondNumber));
+
+  private Function<Double, String> outputFunction = (it) -> String.valueOf(it);
+
 
   @Override
   public Integer add(Integer firstNumber, Integer secondNumber) {
@@ -24,5 +28,10 @@ public class BiFunctionServiceImpl implements BiFunctionService{
   @Override
   public List<String> addNumbersAndTurnAList(Integer firstNumber, Integer secondNumber) {
     return sumAPairNumbertoList.apply(firstNumber,secondNumber);
+  }
+
+  @Override
+  public String powAndTurnString(Integer firstNumber, Integer secondNumber) {
+    return powFunction.andThen(outputFunction).apply(firstNumber,secondNumber);
   }
 }
