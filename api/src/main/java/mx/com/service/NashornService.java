383b13647c8516da8d34526d2fed2f273f25f014
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-09 18:54:19 -0600

    return Double object from the emthod callFunctionsWithFile

--- api/src/main/java/mx/com/service/NashornService.java
+++ api/src/main/java/mx/com/service/NashornService.java
@@ -3,6 +3,6 @@ package mx.com.service;
 public interface NashornService {
   public void callFunctionPrint() throws Exception;
   public void callFunctionPrintWithFile() throws Exception;
-  public void callFunctionsWithFile() throws Exception;
+  public Double callFunctionsWithFile() throws Exception;
   public void implementInterface() throws Exception;
 }
