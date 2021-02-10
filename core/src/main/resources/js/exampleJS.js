f48c6c48c37e3a1ede220ae98e89a785312ec2be
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-09 19:45:55 -0600

    implement a function hiloImplementation

--- core/src/main/resources/js/exampleJS.js
+++ core/src/main/resources/js/exampleJS.js
@@ -3,3 +3,11 @@ print('Hola desde archivo JS');
 function calcular(firstNumber, secondNumber){
   return firstNumber + secondNumber;
 }
+
+var hiloImplementation = {
+  run : function(){
+    for(var i = 0; i < 20;  i ++){
+      print("Print " + i)
+    }
+  }
+}
