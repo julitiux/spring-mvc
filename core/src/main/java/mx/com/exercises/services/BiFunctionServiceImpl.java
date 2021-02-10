3851f8db1f93ffd53b1763dd180c8c9cc3421b30
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-10 15:06:05 -0600

    implementa a method addNumbersAndTurnAList with a BiFunctional sumAPairNumbertoList

--- core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
+++ core/src/main/java/mx/com/exercises/services/BiFunctionServiceImpl.java
@@ -1,11 +1,15 @@
 package mx.com.exercises.services;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
 
 public class BiFunctionServiceImpl implements BiFunctionService{
 
   private BiFunction<Integer, Integer, Integer> addBiFunction = (firstNumber, secondNumber) -> firstNumber+secondNumber;
   private BiFunction<Integer, Integer, Double> powFunction = (firstNumber, secondNumber) -> Math.pow(firstNumber,secondNumber);
+  private BiFunction<Integer, Integer, List<String>> sumAPairNumbertoList = (firstNumber, secondNumber) -> Arrays.asList(String.valueOf(firstNumber + secondNumber));
 
   @Override
   public Integer add(Integer firstNumber, Integer secondNumber) {
@@ -15,5 +19,10 @@ public class BiFunctionServiceImpl implements BiFunctionService{
   @Override
   public Double pow(Integer firstNumber, Integer secondNumber) {
     return powFunction.apply(firstNumber,secondNumber);
+  }
+
+  @Override
+  public List<String> addNumbersAndTurnAList(Integer firstNumber, Integer secondNumber) {
+    return sumAPairNumbertoList.apply(firstNumber,secondNumber);
   }
 }
