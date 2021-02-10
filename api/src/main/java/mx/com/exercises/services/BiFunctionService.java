310c34c366e6daac6610e62953433743d7e97683
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-10 15:05:42 -0600

    declare a interface addNumbersAndTurnAList

--- api/src/main/java/mx/com/exercises/services/BiFunctionService.java
+++ api/src/main/java/mx/com/exercises/services/BiFunctionService.java
@@ -1,8 +1,11 @@
 package mx.com.exercises.services;
 
+import java.util.List;
+
 public interface BiFunctionService {
 
   public Integer add(Integer firstNumber, Integer secondNumber);
   public Double pow(Integer firstNumber, Integer secondNumber);
+  public List<String> addNumbersAndTurnAList(Integer firstNumber, Integer secondNumber);
 
 }
