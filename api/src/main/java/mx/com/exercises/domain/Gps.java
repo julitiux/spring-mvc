4bb2b9d9cfc5a77123cd08bba158d22ef0e6c75d
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-10 22:44:18 -0600

    add new domain for the test

---
+++ api/src/main/java/mx/com/exercises/domain/Gps.java
@@ -0,0 +1,28 @@
+package mx.com.exercises.domain;
+
+public class Gps {
+
+  String latitude;
+  String longitude;
+
+  public Gps(String latitude, String longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+}
