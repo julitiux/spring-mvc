3f877df85a6d0b0ec482fe6119cf843cfa0801ae
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-12 01:10:00 -0600

    declare method findMaxSalary

--- api/src/main/java/mx/com/exercises/services/BinaryOperatorService.java
+++ api/src/main/java/mx/com/exercises/services/BinaryOperatorService.java
@@ -1,5 +1,10 @@
 package mx.com.exercises.services;
 
+import mx.com.exercises.domain.Developer;
+
+import java.util.List;
+
 public interface BinaryOperatorService {
   public Integer addTwoNumbers(Integer firstNumber, Integer secondNumber);
+  public Developer findMaxSalary(List<Developer> list);
 }
