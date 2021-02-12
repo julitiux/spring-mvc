96a39c25c76d6c98cf68fc7501182f6c8d57bb71
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-12 01:09:44 -0600

    implementa a domain Developer, just with username an salary

---
+++ api/src/main/java/mx/com/exercises/domain/Developer.java
@@ -0,0 +1,29 @@
+package mx.com.exercises.domain;
+
+import java.math.BigDecimal;
+
+public class Developer {
+  String username;
+  BigDecimal salary;
+
+  public Developer(String username, BigDecimal salary) {
+    this.username = username;
+    this.salary = salary;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public BigDecimal getSalary() {
+    return salary;
+  }
+
+  public void setSalary(BigDecimal salary) {
+    this.salary = salary;
+  }
+}
