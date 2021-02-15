39c660cef948264d519091d53f990d40ea119cce
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-14 20:31:30 -0600

    declare a method unaryOperatorLikeArgument

--- api/src/main/java/mx/com/exercises/services/UnaryOperatorService.java
+++ api/src/main/java/mx/com/exercises/services/UnaryOperatorService.java
@@ -1,7 +1,11 @@
 package mx.com.exercises.services;
 
+import java.util.List;
+import java.util.function.Function;
+
 public interface UnaryOperatorService {
 
   public Integer unaryOperator(Integer interger);
+  public List<Integer> unaryOperatorLikeArgument(List<Integer> list);
 
 }
