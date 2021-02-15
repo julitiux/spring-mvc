e145f4dbc654db9b6778ecfcf4aa9954a179814d
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-14 20:32:51 -0600

    implemenat a private method that I called unaryOperatorLikeArgument and later I implement the princial method using unaryOperatorLikeArgument and apply

--- core/src/main/java/mx/com/exercises/services/UnaryOperatorServiceImpl.java
+++ core/src/main/java/mx/com/exercises/services/UnaryOperatorServiceImpl.java
@@ -1,6 +1,9 @@
 package mx.com.exercises.services;
 
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 public class UnaryOperatorServiceImpl implements UnaryOperatorService {
 
@@ -10,4 +13,14 @@ public class UnaryOperatorServiceImpl implements UnaryOperatorService {
   public Integer unaryOperator(Integer interger) {
     return functionTheDouble.apply(interger);
   }
+
+  @Override
+  public List<Integer> unaryOperatorLikeArgument(List<Integer> list) {
+    return unaryOperatorLikeArgument(list, it -> it * 2);
+  }
+
+  private List<Integer> unaryOperatorLikeArgument(List<Integer> list, UnaryOperator<Integer> unaryOperator) {
+    return list.stream().map(it -> unaryOperator.apply(it)).collect(Collectors.toList());
+  }
+
 }
