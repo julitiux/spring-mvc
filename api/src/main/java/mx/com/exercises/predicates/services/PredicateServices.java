3ebffb6b4a9e0bee79a09535f2ec8cbfb438025a
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-16 12:41:34 -0600

    declare method filterWithPredicateAnd

--- api/src/main/java/mx/com/exercises/predicates/services/PredicateServices.java
+++ api/src/main/java/mx/com/exercises/predicates/services/PredicateServices.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface PredicateServices {
   public List<Integer> filter(List<Integer> list);
   public List<Integer> filterWithPredicate(List<Integer> list);
+  public List<Integer> filterWithPredicateAnd(List<Integer> list);
 }
