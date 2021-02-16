8618001e8d5402d6a7726cdcf76e55926c2d6f23
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-16 12:14:55 -0600

    implementa a method filterWithPredicate that he used the Predicate majorOrEqualsThanFive, with a lambda expression

--- core/src/main/java/mx/com/exercises/predicates/services/PredicateServicesImpl.java
+++ core/src/main/java/mx/com/exercises/predicates/services/PredicateServicesImpl.java
@@ -1,13 +1,21 @@
 package mx.com.exercises.predicates.services;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class PredicateServicesImpl implements PredicateServices {
 
+  Predicate<Integer> majorOrEqualsThanFive = it -> it >= 5;
+
   @Override
   public List<Integer> filter(List<Integer> list) {
     return list.stream().filter(it -> it >= 5).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Integer> filterWithPredicate(List<Integer> list) {
+    return list.stream().filter(majorOrEqualsThanFive).collect(Collectors.toList());
   }
 
 }
