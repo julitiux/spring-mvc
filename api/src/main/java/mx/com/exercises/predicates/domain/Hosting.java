23894d6f430bb57c9182bc03c72cee29e60760bf
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-16 13:31:27 -0600

    implementat a domain Hosting

---
+++ api/src/main/java/mx/com/exercises/predicates/domain/Hosting.java
@@ -0,0 +1,38 @@
+package mx.com.exercises.predicates.domain;
+
+public class Hosting {
+
+  Long id;
+  String name;
+  String url;
+
+  public Hosting(Long id, String name, String url) {
+    this.id = id;
+    this.name = name;
+    this.url = url;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+}
