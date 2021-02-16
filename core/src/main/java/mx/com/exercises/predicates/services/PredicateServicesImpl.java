5564a5f1cc9655d8cfd40e0f2558b52c12ca1ac2
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-16 12:46:33 -0600

    implement two predicates: lengthEqualsThree and startWithZ. This predicates are to be used in the method filterWithPredicateOr with the element OR

--- core/src/main/java/mx/com/exercises/predicates/services/PredicateServicesImpl.java
+++ core/src/main/java/mx/com/exercises/predicates/services/PredicateServicesImpl.java
@@ -9,6 +9,9 @@ public class PredicateServicesImpl implements PredicateServices {
   Predicate<Integer> majorOrEqualsThanFive = it -> it >= 5;
   Predicate<Integer> majorThanFiveAndMinorThanTen = it -> it > 5 && 10 < it;
 
+  Predicate<String> lengthEqualsThree = it -> it.length() == 3;
+  Predicate<String> startWithZ = it -> it.startsWith("Z");
+
   @Override
   public List<Integer> filter(List<Integer> list) {
     return list.stream().filter(it -> it >= 5).collect(Collectors.toList());
@@ -22,6 +25,11 @@ public class PredicateServicesImpl implements PredicateServices {
   @Override
   public List<Integer> filterWithPredicateAnd(List<Integer> list) {
     return list.stream().filter(majorThanFiveAndMinorThanTen).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<String> filterWithPredicateOr(List<String> list) {
+    return list.stream().filter(lengthEqualsThree.and(startWithZ)).collect(Collectors.toList());
   }
 
 }
