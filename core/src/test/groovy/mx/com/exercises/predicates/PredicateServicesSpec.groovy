13ce43caa047892c071eba1e50523388980e319b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-16 19:15:45 -0600

    implementa a unit test Filter with a predicate

---
+++ core/src/test/groovy/mx/com/exercises/predicates/PredicateServicesSpec.groovy
@@ -0,0 +1,24 @@
+package mx.com.exercises.predicates
+
+import mx.com.exercises.predicates.services.PredicateServicesImpl
+import spock.lang.Shared
+import spock.lang.Specification
+
+class PredicateServicesSpec extends Specification {
+
+  @Shared
+    service = new PredicateServicesImpl()
+
+  @Shared
+  List<Integer> integerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+  def "Filter with a predicate"() {
+    given:
+    List<Integer> response = []
+    when:
+    response = service.predicate(integerList)
+    then:
+    response == integerList.findAll { it >= 5 }
+  }
+}
