e9318a070b4fb415bf1d03f96ac6056a461542dc
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-17 17:54:06 -0600

    declare a method isCorrectTheDomain, send it a param like domainList and BiPredicate

--- api/src/main/java/mx/com/exercises/predicates/services/BiPredicateService.java
+++ api/src/main/java/mx/com/exercises/predicates/services/BiPredicateService.java
@@ -1,5 +1,11 @@
 package mx.com.exercises.predicates.services;
 
+import mx.com.exercises.predicates.domain.Domain;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+
 public interface BiPredicateService {
   public Boolean isTheSameSize(String string, Integer integer);
+  public Boolean isCorrectTheDomain(List<Domain> domainList, BiPredicate<String, Integer> biPredicate);
 }
