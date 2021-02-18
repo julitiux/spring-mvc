ca8cde84d5ca333bc89ce344424a9eabca4a9d6b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-17 18:11:50 -0600

    implement a unit test using a biPredicate #_string | #_size | #_response

---
+++ core/src/test/groovy/mx/com/exercises/predicates/BiPredicateServiceSpec.groovy
@@ -0,0 +1,31 @@
+package mx.com.exercises.predicates
+
+
+import mx.com.exercises.predicates.services.BiPredicateServiceImpl
+import spock.lang.Shared
+import spock.lang.Specification
+
+class BiPredicateServiceSpec extends Specification {
+
+  @Shared
+    service = new BiPredicateServiceImpl()
+
+  def "unit test using a biPredicate #_string | #_size | #_response"() {
+    given:
+    String string = _string
+    Integer size = _size
+    Boolean response
+    when:
+    response = service.isTheSameSize(_string, size)
+    then:
+    response == _response
+    where:
+    _string | _size | _response
+    "julio" | 5     | true
+    "julio" | 4     | false
+    "java"  | 4     | true
+    "java"  | 3     | false
+
+  }
+
+}
