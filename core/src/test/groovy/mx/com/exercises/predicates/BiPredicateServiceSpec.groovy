09000b9222061b83046ef09bae14de45988b8f76
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-17 19:08:05 -0600

    implementing a unit test using a biPredicate with a parameter

--- core/src/test/groovy/mx/com/exercises/predicates/BiPredicateServiceSpec.groovy
+++ core/src/test/groovy/mx/com/exercises/predicates/BiPredicateServiceSpec.groovy
@@ -1,5 +1,6 @@
 package mx.com.exercises.predicates
 
+import mx.com.exercises.predicates.domain.Domain
 import mx.com.exercises.predicates.services.BiPredicateServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
@@ -26,6 +27,27 @@ class BiPredicateServiceSpec extends Specification {
     "julio" | 4     | false
     "java"  | 4     | true
     "java"  | 3     | false
+
+  }
+
+  @Shared
+    domainList = [new Domain("google.com", 1),
+                  new Domain("i-am-spammer.com", 10),
+                  new Domain("github.com", 0),
+                  new Domain("microsoft.com", 2)]
+
+  def "unit test using a biPredicate with a parameter"() {
+    given:
+    List<Domain> response = []
+    when:
+    response = service.isCorrectTheDomain(domainList, _lambda)
+    then:
+    response == _response
+    where:
+    _lambda                                                  | _response
+    service.isCorrectUrl                                     | domainList.findAll { it.name.equalsIgnoreCase("google.com") || it.score == 0 }
+    { domain, score -> score == 0 }                          | domainList.findAll { it.score == 0 }
+    { domain, score -> domain.startsWith("i") && score > 5 } | domainList.findAll { it.name.startsWith("i") && it.score > 5 }
 
   }
 
