2b329874f81f8b4f5494c6f004bea6fef929c41c
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-18 22:01:17 -0600

    declare the method usingAConsumerWithAInteger

--- api/src/main/java/mx/com/exercises/consumers/services/ConsumerService.java
+++ api/src/main/java/mx/com/exercises/consumers/services/ConsumerService.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface ConsumerService {
   public void outPrintln(List<String> list);
   public void usingAConsumer(List<String> list);
+  public void usingAConsumerWithAInteger(List<Integer> list);
 }
