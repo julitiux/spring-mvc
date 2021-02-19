7506074f2ed8fb6e2cc97d1046097903dc56182e
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-18 21:43:49 -0600

    implement a Consumer print using a System.out.println

--- core/src/main/java/mx/com/exercises/consumers/services/ConsumerServiceImpl.java
+++ core/src/main/java/mx/com/exercises/consumers/services/ConsumerServiceImpl.java
@@ -1,8 +1,11 @@
 package mx.com.exercises.consumers.services;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ConsumerServiceImpl implements ConsumerService {
+
+  Consumer<String> print = it -> System.out.println(it);
 
   @Override
   public void outPrintln(List<String> list) {
