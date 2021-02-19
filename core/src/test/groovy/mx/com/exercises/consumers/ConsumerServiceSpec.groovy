b8ec077d9003e6e195300bb273b6a33ac1f340a9
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-19 16:09:20 -0600

    test that implement a Consumer with a function that recive a List of Integers

--- core/src/test/groovy/mx/com/exercises/consumers/ConsumerServiceSpec.groovy
+++ core/src/test/groovy/mx/com/exercises/consumers/ConsumerServiceSpec.groovy
@@ -10,16 +10,24 @@ class ConsumerServiceSpec extends Specification {
     service = new ConsumerServiceImpl()
 
   @Shared
-  List<String> list = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez']
+  List<String> stringList = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez']
+
+  @Shared
+  List<Integer> integerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   def "test that Consumer in a normal forEach with a expression lambda"() {
     expect:
-    service.outPrintln(list)
+    service.outPrintln(stringList)
   }
 
   def "test that implement a Consumer with a Consumer print"() {
     expect:
-    service.usingAConsumer(list)
+    service.usingAConsumer(stringList)
+  }
+
+  def "test that implement a Consumer with a function that recive a List of Integers"() {
+    expect:
+    service.usingAConsumerWithAInteger(integerList)
   }
 
 }
