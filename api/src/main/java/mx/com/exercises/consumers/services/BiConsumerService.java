e640f9cb6239a9d9921fbc0eae968ffc0218d7ed
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-20 21:07:39 -0600

    declare a method highOrderFunction

--- api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
+++ api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface BiConsumerService {
   public void biConsumer(Integer integerOne, Integer integerTwo);
+  public void highOrderFunction(Integer integerOne, Integer integerTwo);
 }
