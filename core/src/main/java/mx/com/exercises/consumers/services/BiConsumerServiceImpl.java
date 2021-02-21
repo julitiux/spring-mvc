88a06ad5e5f17720c05ab4796fca10307f718c87
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-20 21:08:38 -0600

    implement a method addTwoNumbers using a BiConsumer like parameter and send it with accept. The lambda is (x, y) -> System.out.print(x + y)

--- core/src/main/java/mx/com/exercises/consumers/services/BiConsumerServiceImpl.java
+++ core/src/main/java/mx/com/exercises/consumers/services/BiConsumerServiceImpl.java
@@ -10,4 +10,14 @@ public class BiConsumerServiceImpl implements BiConsumerService {
   public void biConsumer(Integer integerOne, Integer integerTwo) {
     addTwoNumber.accept(integerOne, integerTwo);
   }
+
+  @Override
+  public void highOrderFunction(Integer integerOne, Integer integerTwo) {
+    addTwoNumbers(integerOne, integerTwo, (x, y) -> System.out.print(x + y));
+  }
+
+  void addTwoNumbers(Integer integerOne, Integer integerTwo, BiConsumer<Integer, Integer> biConsumer) {
+    biConsumer.accept(integerOne, integerTwo);
+  }
+
 }
