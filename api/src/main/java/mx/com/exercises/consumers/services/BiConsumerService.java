11324b406631f9d9bc0564885713c70f892b088c
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-21 21:17:46 -0600

    delete a import usless

--- api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
+++ api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
@@ -1,7 +1,5 @@
 package mx.com.exercises.consumers.services;
 
-import java.util.List;
-
 public interface BiConsumerService {
   public void biConsumer(Integer integerOne, Integer integerTwo);
   public void highOrderFunction(Integer integerOne, Integer integerTwo);
