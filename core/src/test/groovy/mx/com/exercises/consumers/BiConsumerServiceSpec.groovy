8b216c4936669c4e7801ea7acafde57ebe1dc833
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-02-22 15:40:19 -0600

    implementing a unit test using a CiConsumer with accept

--- core/src/test/groovy/mx/com/exercises/consumers/BiConsumerServiceSpec.groovy
+++ core/src/test/groovy/mx/com/exercises/consumers/BiConsumerServiceSpec.groovy
@@ -1,11 +1,20 @@
 package mx.com.exercises.consumers
 
+import mx.com.exercises.consumers.services.BiConsumerServiceImpl
+import spock.lang.Shared
 import spock.lang.Specification
 
-class BiConsumerServiceSpec extends Specification{
+class BiConsumerServiceSpec extends Specification {
 
-  def""(){
+  @Shared
+    service = new BiConsumerServiceImpl()
 
+  def "unit test using a CiConsumer with accept"() {
+    given:
+    Integer integerOne = 1
+    Integer integerTwo = 2
+    expect:
+    service.highOrderFunction(integerOne, integerTwo)
   }
 
 }
