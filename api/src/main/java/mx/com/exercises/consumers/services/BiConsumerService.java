31314c33955b49bb8fa1e99c0f9ad7a9c5f8bdac
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-01 16:56:38 -0600

    declare new method mapForEach, just for overwrite

--- api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
+++ api/src/main/java/mx/com/exercises/consumers/services/BiConsumerService.java
@@ -1,6 +1,9 @@
 package mx.com.exercises.consumers.services;
 
+import java.util.Map;
+
 public interface BiConsumerService {
   public void biConsumer(Integer integerOne, Integer integerTwo);
   public void highOrderFunction(Integer integerOne, Integer integerTwo);
+  public void mapForEach(Map<Integer, String> map);
 }
