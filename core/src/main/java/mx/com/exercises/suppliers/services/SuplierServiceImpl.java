1994f826f04f15aeea206cd3c4ff65a63ecb5624
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-01 20:38:28 -0600

    change the supplier by another one, the format

--- core/src/main/java/mx/com/exercises/suppliers/services/SuplierServiceImpl.java
+++ core/src/main/java/mx/com/exercises/suppliers/services/SuplierServiceImpl.java
@@ -3,15 +3,19 @@ package mx.com.exercises.suppliers.services;
 import mx.com.exercises.suppliers.SuplierService;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Supplier;
 
 public class SuplierServiceImpl implements SuplierService {
 
+  DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
   Supplier<LocalDateTime> now = () -> LocalDateTime.now();
+  Supplier<String> format = () -> dtf.format(LocalDateTime.now());
 
   @Override
   public void nowDateFormat() {
-    System.out.println(now.get());
+    System.out.println(format.get());
   }
 
 }
