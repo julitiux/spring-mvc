d2437c0349fe6a143f46a0059f10e50931606eaf
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-02 20:18:00 -0600

    implement a method filterPersonByAge. This method recive a age an filter a personList with the parameter age. Return a Person List

--- core/src/main/java/mx/com/exercises/streams/services/FilterAndCollectServiceImpl.java
+++ core/src/main/java/mx/com/exercises/streams/services/FilterAndCollectServiceImpl.java
@@ -1,5 +1,7 @@
 package mx.com.exercises.streams.services;
 
+import mx.com.exercises.streams.domain.Person;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +10,11 @@ public class FilterAndCollectServiceImpl implements FilterAndCollectService {
   @Override
   public List<String> filterStartWith(List<String> stringList, String filter) {
     return stringList.stream().filter(it -> it.startsWith(filter)).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Person> filterPersonByAge(List<Person> stringList, Integer age) {
+    return stringList.stream().filter(person -> person.getAge() > age).collect(Collectors.toList());
   }
 
 }
