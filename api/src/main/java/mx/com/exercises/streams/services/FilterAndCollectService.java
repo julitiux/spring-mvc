1bf132e15d88cc6bd34a6b5c3100f437b24c55b3
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-02 20:16:56 -0600

    declare a method filterPersonByAge

--- api/src/main/java/mx/com/exercises/streams/services/FilterAndCollectService.java
+++ api/src/main/java/mx/com/exercises/streams/services/FilterAndCollectService.java
@@ -1,7 +1,10 @@
 package mx.com.exercises.streams.services;
 
+import mx.com.exercises.streams.domain.Person;
+
 import java.util.List;
 
 public interface FilterAndCollectService {
   public List<String> filterStartWith(List<String> stringList, String filter);
+  public List<Person> filterPersonByAge(List<Person> stringList, Integer age);
 }
