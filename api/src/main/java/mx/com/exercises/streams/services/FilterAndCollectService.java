1e76ed516c5b8ab71dcfd8f634178548debb2402
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-02 21:04:47 -0600

    declare the method getListOfUsernamePerson

--- api/src/main/java/mx/com/exercises/streams/services/FilterAndCollectService.java
+++ api/src/main/java/mx/com/exercises/streams/services/FilterAndCollectService.java
@@ -8,4 +8,5 @@ public interface FilterAndCollectService {
   public List<String> filterStartWith(List<String> stringList, String filter);
   public List<Person> filterPersonByAge(List<Person> personList, Integer age);
   public Person filterPersonUsingAny(List<Person> personList, Integer age);
+  public List<String> getListOfUsernamePerson(List<Person> personList);
 }
